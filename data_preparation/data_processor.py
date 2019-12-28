3ffa0856b130048b23fd8ae2ecfae263c39c2550
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2019-12-29 00:30:10 +0100

    init

--- data_preparation/data_processor.py
+++ data_preparation/data_processor.py
@@ -2,7 +2,7 @@ from statistics import mean
 
 input_words = []
 input_words_reduced = []
-corpus_words = []
+czech_corpus_words = []
 czech_stopwords = ['a', 'u', 'v', 'aby', 's', 'o', 'jak', 'to', 'ale', 'za', 've', 'i', ',',
                    'ja', 'ke', 'co', 'je', 'z', 'sa', 'na', 'ze', 'ač', 'že', 'či', 'který',
                    'nějaký', 'pouze', 'bez', 'si', 'jsem', 'já', 'jí', 'by', 'asi', 'být',
@@ -22,7 +22,16 @@ czech_stopwords = ['a', 'u', 'v', 'aby', 's', 'o', 'jak', 'to', 'ale', 'za', 've
                    'tak', 'ma', 'pri', 'od', 'po', 'jsou', 'jak', 'dalsi', 'ale', 'si', 've', 'to',
                    'jako', 'za', 'zpet', 'ze', 'do', 'pro', 'je', 'na']
 
-def word_valence_calculator(input_file_path, output_file_path):
+
+# 2 reduce the words list of lists to remove czech stopwords
+# and remove words not in the czech adjectives corpus file
+def _read_file_Generator(temp_file_path):
+    file = temp_file_path
+    for row in open(file, encoding="utf8"):
+        yield row[:-1]
+
+
+def word_valence_calculator(input_file_path, temp_file_path, output_file_path):
     """
     function for mean valence value calculation for each word
     :return: 0
@@ -37,16 +46,24 @@ def word_valence_calculator(input_file_path, output_file_path):
             except IndexError:
                 pass
 
-    # 2 reduce the words list of lists to remove czech stopwords
-    # and remove words not in the czech adjectives corpus file
-    with open('./data_input/czech_adjectives_only.txt', 'r', encoding='utf8') as c:
-        for line in c:
-            corpus_words.append(line)
+    print('step #1 completed')
+
+    corpus_gen = _read_file_Generator(temp_file_path)
+    for cg in corpus_gen:
+        czech_corpus_words.append(cg)
+
+
+    # with open(temp_file_path, 'r', encoding='utf8') as c:
+    #     for line in c:
+    #         czech_corpus_words.append(line)
+
 
     for item in input_words:
         if item not in czech_stopwords:
-            if item in corpus_words:
+            if item in czech_corpus_words:
                 input_words_reduced.append(item)
+
+    print('step #2 completed')
 
     # 3 calculate the mean valence for each word
     dict = {}
@@ -58,16 +75,21 @@ def word_valence_calculator(input_file_path, output_file_path):
     for key in dict:
         dict[key] = [mean(i) for i in zip(*dict[key])]
 
+    print('step #3 completed')
+
     # 4 save the calculated results to the output_file_path text file
     with open(output_file_path, 'w', encoding='utf8') as fw:
         for key, value in dict.items():
             fw.write(key + ' ' + str(int(value[0])) + '\n')
 
+    print('step #4 completed')
+
     return 0
 
 
 if __name__ == "__main__":
-    res = word_valence_calculator(input_file_path = './data_temp/temp_file.txt',
+    res = word_valence_calculator(input_file_path = './data_input/czech_adjectives_only.txt',
+                                  temp_file_path = './data_temp/temp_file.txt',
                                   output_file_path = './data_output/czech_adjectives_only_rated_for_valence.txt')
     if res == 0:
         print("Data processing phase complete.")
