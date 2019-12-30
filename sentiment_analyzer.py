aeff2afe1c0ec0daa2b56cdec832063956735475
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2019-12-31 00:09:57 +0100

    init

---
+++ sentiment_analyzer.py
@@ -0,0 +1,100 @@
+"""
+sentiment analyzer CLI tool
+"""
+import argparse
+import logging
+
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+LOGGER = logging.getLogger(__name__)
+
+
+def _read_valence_file_generator(valence_file):
+    """
+    read valence file generator
+    :return: yield rows
+    """
+    for row in open(valence_file, encoding="utf8"):
+        yield row[:-1]
+
+
+def read_valence_file(level):
+    """
+    function opening the expected valence file based on the level input
+    and populating a dictionary with the words and their corresponding valence
+    values
+    :param level:
+    :return:
+    """
+    if level == "low":
+        valence_file = open("./data_preparation/data_output/"
+                            "small_czech_words_list_w_word_valence.txt", encoding='utf8')
+
+    elif level == "high":
+        valence_file = _read_valence_file_generator("./data_preparation/data_output/"
+                                                    "big_czech_words_list_w_word_valence.txt")
+
+    else:
+        raise FileNotFoundError
+
+    scores = {}
+    for line in valence_file:
+        term, score = line.split()
+        scores[term] = float(score)
+
+    LOGGER.info('file with word and corresponding valence pairs loaded successfully')
+
+    return scores.items()
+
+
+def get_sentiment(prepared_args):
+    """
+    function calculating the sentiment score
+    :param prepared_args:
+    :return: sentiment value
+    """
+    valence_file = read_valence_file(prepared_args['level'])
+    words = prepared_args['string'].split()
+    sentiment_stack = 0.0
+
+    for word in words:
+        for item in valence_file:
+            if word in item:
+                sentiment_stack += item[1]
+
+    LOGGER.info('Sentiment calculated successfully')
+
+    return sentiment_stack
+
+
+
+def prepare_args():
+    """
+    function for preparation of the CLI arguments
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-string', '--inputstring', type=str, required=True)
+    parser.add_argument('-level', '--depthlevel', type=str, required=False, default='low',
+                        choices=['low', 'high'])
+    parser.add_argument('-fuzzy', '--fuzzymatch', type=bool, required=False, default=False)
+    parsed = parser.parse_args()
+
+    string = parsed.inputstring
+    level = parsed.depthlevel
+    fuzzy = parsed.fuzzymatch
+
+    LOGGER.info('arguments parsed successfully')
+
+    return {'string': string,
+            'level': level,
+            'fuzzy': fuzzy}
+
+
+if __name__ == '__main__':
+    PREPARED_ARGS = prepare_args()
+    SENTIMENT_RESULT = get_sentiment(PREPARED_ARGS)
+    LOGGER.info('Sentiment value is: {}'.format(SENTIMENT_RESULT))
