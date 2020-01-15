f1cbea7c03f2302b89b54063cc6bead7144990b3
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2020-01-15 23:38:48 +0100

    cleanups

--- utils/utils.py
+++ utils/utils.py
@@ -3,17 +3,15 @@ project common helper functions
 """
 
 
-CZECH_STOPWORDS_FILE_PATH = '../data_preparation/czech_stopwords.txt'
-
-
-def _read_czech_stopwords() -> list:
+def _read_czech_stopwords(czech_stopwords_file_path) -> list:
     """
     function reading czech stopwords file and storing it to a list
+    :param czech_stopwords_file_path
     :return:czech_stopwords
     """
     czech_stopwords = []
 
-    with open(CZECH_STOPWORDS_FILE_PATH, 'r', encoding='utf8') as stop_word_file:
+    with open(czech_stopwords_file_path, 'r', encoding='utf8') as stop_word_file:
         for line in stop_word_file:
             czech_stopwords.append(line[:-1])
 
@@ -28,8 +26,10 @@ def _replace_all(text) -> str:
     """
     replacements = {'"': '', '.': '', '(': '', ')': '', ',': '',
                     '-': '', '?': '', '!': '', ':': '', '/': '',
-                    '„': '' , '<strong>': '', '<em>': ''}
-    #TODO replace ě : e, š : s... and retrain models and scrub the input text against this function
+                    '„': '' , '<strong>': '', '<em>': '', 'ě': 'e',
+                    'š': 's', 'č': 'c', 'ř': 'r', 'ž': 'z', 'ý':'y',
+                    'á': 'a', 'í': 'i', 'é': 'e', 'ů': 'u', 'ú': 'u'}
+
     for i, j in replacements.items():
         text = text.replace(i, j)
     return text
