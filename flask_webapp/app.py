e10bb8e3150e6f80e281659fdf939a13b34de03d
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2020-02-09 18:27:27 +0100

    heroku procfile

--- flask_webapp/app.py
+++ flask_webapp/app.py
@@ -306,4 +306,4 @@ def stats(period="week"):
 
 if __name__ == "__main__":
     #serve(app, host='0.0.0.0', port=80, threads=THREADS_COUNT)
-    serve(app)
+    serve(app, host='127.0.0.1', port=5000)
