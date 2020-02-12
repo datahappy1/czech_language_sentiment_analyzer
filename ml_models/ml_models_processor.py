36c6d9ae1133971e0d106d71a4a6fe78bf0c0ffd
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2020-02-12 22:29:16 +0100

    heroku procfile

---
+++ ml_models/ml_models_processor.py
@@ -0,0 +1,12 @@
+"""
+the ml_models_processor module allows to update all of the used ml models from one location
+"""
+from ml_models.naive_bayes import data_processor_naive_bayes
+from ml_models.logistic_regression import data_processor_logistic_regression
+from ml_models.support_vector_machine import data_processor_support_vector_machine
+
+PERSIST_MODEL_TO_FILE = False
+
+data_processor_naive_bayes.naive_bayes(PERSIST_MODEL_TO_FILE)
+data_processor_logistic_regression.logistic_regression(PERSIST_MODEL_TO_FILE)
+data_processor_support_vector_machine.support_vector_machine(PERSIST_MODEL_TO_FILE)
