1fb4d03f69eaad8ed28aff512cd88a3d22b0c8cd
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2020-05-06 00:40:45 +0200

    polishing js

---
+++ flask_webapp/static/js/template_functions.js
@@ -0,0 +1,17 @@
+function show_details() {
+    if(document.getElementById('span_with_details').style.display=='none') {
+        document.getElementById('span_with_details').style.display='block';
+        document.getElementById('link_show_details').style.display='none';
+    }
+    return false;
+}
+
+function clear_textarea_and_alert() {
+    document.getElementById('Input_Text').value = '';
+    if (document.getElementById('error_message')) {
+        document.getElementById('error_message').style.display='none';
+    }
+    if (document.getElementById('result_overall_sentiment')) {
+    document.getElementById('result_overall_sentiment').style.display='none';
+    }
+}
